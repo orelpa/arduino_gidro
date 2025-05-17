98692e97225dfc40c4e33a796b287b8653db7de0
Platon Orel <pa.orel@yandex.ru>
2025-05-17 18:54:53 +0300

    Добавлено реле, на пин 2

--- ver_1/sketch_may14b/sketch_may14b.ino
+++ ver_1/sketch_may14b/sketch_may14b.ino
@@ -1,3 +1,4 @@
+#define RELAY_IN 2
 const int AquaLevelPin = A0;
 
 int AquaOutput = 0;
@@ -5,8 +6,10 @@ int AquaOutput = 0;
 void setup() {
   // Инициализируем серийный порт со скоростью 9600 бит/с
   Serial.begin(9600);
+  //пин реле как выход
+  pinMode (RELAY_IN, OUTPUT);
 }
-//test
+
 
 void loop() {
   // Читаем значение с датчика уровня жидкости
@@ -16,6 +19,8 @@ void loop() {
   Serial.print("Уровень жидкости: ");
   Serial.println(AquaOutput);
   
+  digitalWrite(RELAY_IN, HIGH);
   // Ждем 1 секунду перед следующим чтением
   delay(1000);
+  digitallWrite(RELAY_IN, LOW);
 }
