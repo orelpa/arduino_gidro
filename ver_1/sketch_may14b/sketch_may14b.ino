d1b4d274e27230214a8ff272e5a8cb20a4673c5d
Platon Orel <pa.orel@yandex.ru>
2025-06-01 19:58:23 +0300

    Добавлен второй датчик, отключено управление реле

--- ver_1/sketch_may14b/sketch_may14b.ino
+++ ver_1/sketch_may14b/sketch_may14b.ino
@@ -1,7 +1,11 @@
 #define RELAY_IN 2
-const int AquaLevelPin = A0;
+const int AquaLevelPinUp = A0;// s - желтый
+const int AquaLevelPinDown = A1;// s - желтый
 
-int AquaOutput = 0;
+int AquaOutputUp = 0;
+int AquaOutputDown = 0;
+
+int aquaStart = 0;
 
 void setup() {
   // Инициализируем серийный порт со скоростью 9600 бит/с
@@ -12,22 +16,28 @@ void setup() {
 
 
 void loop() {
-  // Читаем значение с датчика уровня жидкости
-  int AquaLevel = analogRead(AquaLevelPin);
-  AquaOutput = map(AquaLevel, 0, 370, 0, 10);
+  // Читаем значение с датчиков уровня жидкости верхнего и нижнего
+  int AquaLevelUp = analogRead(AquaLevelPinUp);
+  int AquaLevelDown = analogRead(AquaLevelPinDown);
+
+
+  AquaOutputUp = map(AquaLevelUp, 0, 370, 0, 10);
+  AquaOutputDown = map(AquaLevelDown, 0, 370, 0, 10);
   // Выводим значение в Serial Monitor
-  Serial.print("Уровень жидкости: ");
-  Serial.println(AquaOutput);
-  
+  Serial.print("Верхний уровень жидкости: ");
+  Serial.println(AquaOutputUp);
+  Serial.print("Нижний уровень жидкости: ");
+  Serial.println(AquaOutputDown);
+  delay (1000);
   //digitalWrite(RELAY_IN, HIGH);
   // Ждем 1 секунду перед следующим чтением
-  delay(1000);
-  if (AquaOutput < 4){
-    digitalWrite(RELAY_IN, HIGH); // включаем реле
-  }
-  else{
-    digitalWrite(RELAY_IN, LOW);
-  }
+  //delay(1000);
+  //if (AquaOutput < 4){
+  //  digitalWrite(RELAY_IN, HIGH); // включаем реле
+  //}
+  //else{
+  //  digitalWrite(RELAY_IN, LOW);
+  //}
   //digitalWrite(RELAY_IN, LOW);
   //delay(1000);
 }
